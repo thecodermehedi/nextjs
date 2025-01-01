d8c2a93b7d890adbfc5882a8f37f55c016a543e6
Mehedi Hasan <thecodermehedi@gmail.com>
2025-01-01 23:18:14 +0600

    Enhance Home component layout with flexbox for better alignment and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+ return (
+  <div className="min-h-screen flex items-center justify-center">
+   <h1 className="text-2xl">Home</h1>
+  </div>
+ );
 };
 
 export default Home;
